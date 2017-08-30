9dcd0d138f2c1f2f9432193b37210b82a158f433
koverton <ken.overton@gmail.com>
2017-08-30 11:05:11 -0400

    Fix usage output

--- bin/run-bridge.sh
+++ bin/run-bridge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
 if [ "$#" -lt 3 ]; then
-	echo "	USAGE: $0 <solace-bridge-queue-to-kafka> <kafka> <topic> <list> <..."
+	echo "	USAGE: $0 <path/to/client-connect.properties> <solace-bridge-queue> <kafka-bridge-topic-list> <...>"
 	echo ""
 	exit 1
 fi
 cd `dirname $0`/..
 
 cp=`mvn -q exec:exec -Dexec.executable=echo -Dexec.args="%classpath"`
-java -cp "$cp" com.solacesystems.poc.BridgingConnector $1 $2 $3
+java -cp "$cp" com.solacesystems.poc.BridgingConnector $*
